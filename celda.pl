4fb128091d68bf7a669f530690a44b0ae6f36279
Miguel Perez <miguepersa@gmail.com>
2023-12-14 21:30:31 -0400

    testing ongonig

--- celda.pl
+++ celda.pl
@@ -3,32 +3,38 @@ cruzar(Mapa, Palancas, Seguro) :-
     % Verificar que Mapa esté instanciado y no sea una variable
     nonvar(Mapa),
     % Verificar que Palancas esté instanciado o sea una variable
-    (var(Palancas); nonvar(Palancas)),
-    % Verificar que Seguro esté instanciado o sea una variable
-    (var(Seguro); nonvar(Seguro)),
-    % Llamar al predicado auxiliar cruzar_aux/4 para realizar la verificación
     cruzar_aux(Mapa, Palancas, Seguro, []).
 
 % Caso base: cruzar un pasillo regular con la palanca hacia arriba
 cruzar_aux(pasillo(X, regular), Palancas, seguro, _) :-
     member((X, arriba), Palancas).
+cruzar_aux(pasillo(X, regular), Palancas, trampa, _) :-
+    member((X, abajo), Palancas).
 
 % Caso base: cruzar un pasillo de cabeza con la palanca hacia abajo
-cruzar_aux(pasillo(X, cabeza), Palancas, seguro, _) :-
+cruzar_aux(pasillo(X, de_cabeza), Palancas, seguro, _) :-
     member((X, abajo), Palancas).
+cruzar_aux(pasillo(X, de_cabeza), Palancas, trampa, _) :-
+    member((X, arriba), Palancas).
 
 % Caso recursivo: cruzar una junta (secuencia de dos submapas)
 cruzar_aux(junta(SubMapa1, SubMapa2), Palancas, Seguro, Visitados) :-
     cruzar_aux(SubMapa1, Palancas, Seguro1, Visitados),
     cruzar_aux(SubMapa2, Palancas, Seguro2, Visitados),
+    % Si cruzar ambos submapas es seguro, entonces es seguro
     (Seguro1 = seguro, Seguro2 = seguro -> Seguro = seguro ; Seguro = trampa).
 
-
 % Caso recursivo: cruzar una bifurcación (cualquiera de los dos submapas es suficiente)
-cruzar_aux(bifurcacion(SubMapa1, _), Palancas, Seguro, Visitados) :-
-    cruzar_aux(SubMapa1, Palancas, Seguro, Visitados).
-cruzar_aux(bifurcacion(_, SubMapa2), Palancas, Seguro, Visitados) :-
-    cruzar_aux(SubMapa2, Palancas, Seguro, Visitados).
+cruzar_aux(bifurcacion(SubMapa1, SubMapa2), Palancas, Seguro, Visitados) :-
+    % Intentar cruzar el primer submapa
+    cruzar_aux(SubMapa1, Palancas, Seguro1, Visitados),
+    % Si cruzar el primer submapa es seguro, entonces es seguro
+    (Seguro1 = seguro -> Seguro = seguro
+    % Si no, intentar cruzar el segundo submapa
+    ; cruzar_aux(SubMapa2, Palancas, Seguro2, Visitados),
+      % Si cruzar el segundo submapa es seguro, entonces es seguro
+      (Seguro2 = seguro -> Seguro = seguro ; Seguro = trampa)
+    ).
 
 % Verificar si existe alguna combinación de palancas que permita cruzar el laberinto
 % (usando la técnica de backtracking)
@@ -38,11 +44,16 @@ cruzar_aux(_, _, seguro, _).
 cruzar_aux(_, _, trampa, _) :-
     fail.
 
+
 % Predicado para manejar el caso de bifurcaciones en el laberinto
-cruzar_aux(SubMapa, Palancas, Seguro, Visitados) :-
+cruzar_aux(SubMapa, Palancas, Seguro, Visitados, RVisited) :-
     SubMapa \= bifurcacion(_, _),
     \+ member(SubMapa, Visitados),
-    cruzar_aux(SubMapa, Palancas, Seguro, [SubMapa | Visitados]).
+    % Añadir SubMapa a la lista de visitados para este nivel
+    cruzar_aux(SubMapa, Palancas, Seguro, [SubMapa | Visitados], RVisited),
+    % Añadir SubMapa a la lista de visitados global
+    cruzar_aux(SubMapa, Palancas, Seguro, [SubMapa | RVisited], RVisited).
+
 
 % Definición del predicado siempre_seguro/1
 siempre_seguro(Mapa) :-
